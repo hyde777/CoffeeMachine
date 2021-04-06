04566bc298c90d5d1d73377f951bffb6ac23cf5e
yohan fairfort <yohan.fairfort@gmail.com>
2021-04-06 19:42:11 +0200

    Refacto from switch return to Dictionary since it's injectable

--- CoffeeMachine/CoffeePricer.cs
+++ CoffeeMachine/CoffeePricer.cs
@@ -1,17 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using CoffeeMachine;
 
 namespace CoffeeMachine
 {
     public class CoffeePricer : ICoffeePricer
     {
-        public decimal Command(string beverage) =>
-            beverage switch
+        private readonly Dictionary<string, IBeverage> _recipes;
+
+        public CoffeePricer()
+        {
+            _recipes = new()
             {
-                "Expresso" => new Beverage(new List<IIngredient> {new Coffee(), new Water()}).Price(),
-                "Cappucino" => new Beverage(new List<IIngredient>{new Coffee(), new Water(), new Cream(), new Chocolate()}).Price(),
-                "Allongé" => new Beverage(new List<IIngredient> {new Coffee(), new Water(), new Water()}).Price(),
-                _ => throw new Exception()
+                {"Expresso", new Beverage(new List<IIngredient> {new Coffee(), new Water()})},
+                {
+                    "Cappucino", new Beverage(new List<IIngredient>
+                        {new Coffee(), new Water(), new Cream(), new Chocolate()})
+                },
+                {"Allongé", new Beverage(new List<IIngredient> {new Coffee(), new Water(), new Water()})},
             };
+        }
+
+        public decimal Command(string beverage) => _recipes.FirstOrDefault(b => b.Key == beverage).Value.Price();
     }
 }
