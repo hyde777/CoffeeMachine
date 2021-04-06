b7c3054328c3f514e38e3f2f6ba5a25954003193
yohan fairfort <yohan.fairfort@gmail.com>
2021-04-06 18:15:25 +0200

    Expresso Test

---
+++ CoffeeMachineTests/CoffeeMachineTests.cs
@@ -0,0 +1,34 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace CoffeeMachineTests
+{
+    public class AcceptanceTests
+    {
+        [Test]
+        public void METHOD()
+        {
+            decimal waterPrice = new(0.2);
+            decimal coffeePrice = new(1);
+            ICoffeePricer pricer = new CoffeePricer();
+
+            decimal price = pricer.Command("Expresso");
+
+            decimal expressoPrice = waterPrice + coffeePrice;
+            price.Should().Be(expressoPrice);
+        }
+    }
+
+    public class CoffeePricer : ICoffeePricer
+    {
+        public decimal Command(string beverage)
+        {
+            return new(1.2);
+        }
+    }
+
+    public interface ICoffeePricer
+    {
+        decimal Command(string beverage);
+    }
+}
